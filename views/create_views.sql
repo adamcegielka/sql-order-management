69b7513195a6aeace47addd01fa175e324a8dd89
Adam <adamcegielka@tlen.pl>
2023-07-06 13:07:54 +0200

    styles: change language in `create_views.sql` file

--- views/create_views.sql
+++ views/create_views.sql
@@ -1,10 +1,10 @@
--- Tworzenie widoku, który pokazuje informacje o zamówieniach dla każdego klienta
+-- Creation of a view that shows order information for each customer
 CREATE VIEW CustomerOrders AS
 SELECT Customers.CustomerID, Customers.FirstName, Customers.LastName, Orders.OrderDate, Orders.TotalAmount
 FROM Customers
 JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
 
--- Pobieranie informacji o zamówieniach dla klienta z widoku
+-- Retrieving order information for a customer from a view
 SELECT FirstName, LastName, OrderDate, TotalAmount
 FROM CustomerOrders
 WHERE CustomerID = 1;
