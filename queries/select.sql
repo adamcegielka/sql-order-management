c206cb75243d1898e1cd4b4907e725f7607b14b2
Adam <adamcegielka@tlen.pl>
2023-07-14 18:58:43 +0200

    fix(style): Improve code view

--- queries/select.sql
+++ queries/select.sql
@@ -1,33 +1,76 @@
 -- Customers:
 SELECT * FROM Customers;
-SELECT DISTINCT * FROM Customers;
-SELECT FirstName FROM Customers;
-SELECT DISTINCT FirstName FROM Customers;
-SELECT City FROM Customers;
-SELECT DISTINCT City FROM Customers;
 
+SELECT DISTINCT *
+FROM Customers;
+
+SELECT FirstName, LastName
+FROM Customers;
+
+SELECT DISTINCT FirstName
+FROM Customers;
+
+SELECT FirstName, LastName, City
+FROM Customers;
+
+SELECT DISTINCT City
+FROM Customers;
+
+-- ------------------------------------------------------
 -- Orderitems:
 SELECT * FROM Orderitems;
-SELECT DISTINCT * FROM Orderitems;
-SELECT Quantity FROM Orderitems;
-SELECT DISTINCT Quantity FROM Orderitems;
-SELECT Price FROM Orderitems;
-SELECT DISTINCT Price FROM Orderitems;
 
+SELECT DISTINCT *
+FROM Orderitems;
+
+SELECT OrderItemID, Quantity
+FROM Orderitems;
+
+SELECT DISTINCT Quantity
+FROM Orderitems;
+
+SELECT OrderItemID, Quantity, Price
+FROM Orderitems;
+
+SELECT DISTINCT Price
+FROM Orderitems;
+
+-- ------------------------------------------------------
 -- Orders:
 SELECT * FROM Orders;
-SELECT DISTINCT * FROM Orders;
-SELECT CustomerID FROM Orders;
-SELECT DISTINCT CustomerID FROM Orders;
-SELECT TotalAmount FROM Orders;
-SELECT DISTINCT TotalAmount FROM Orders;
 
+SELECT DISTINCT *
+FROM Orders;
+
+SELECT CustomerID, TotalAmount
+FROM Orders;
+
+SELECT DISTINCT CustomerID
+FROM Orders;
+
+SELECT OrderID, TotalAmount
+FROM Orders;
+
+SELECT DISTINCT TotalAmount
+FROM Orders;
+
+-- ------------------------------------------------------
 -- Products:
 SELECT * FROM Products;
-SELECT DISTINCT * FROM Products;
-SELECT UnitPrice FROM Products;
-SELECT DISTINCT UnitPrice FROM Products;
-SELECT QuantityInStock FROM Products;
-SELECT DISTINCT QuantityInStock FROM Products;
+
+SELECT DISTINCT *
+FROM Products;
+
+SELECT ProductID, UnitPrice
+FROM Products;
+
+SELECT DISTINCT UnitPrice
+FROM Products;
+
+SELECT ProductID, UnitPrice, QuantityInStock
+FROM Products;
+
+SELECT DISTINCT QuantityInStock
+FROM Products;
 
 -- DISTINCT - Query without duplicates
