9078dfe7ece07369dde89b0cd8870e38cff6e8e1
Adam <adamcegielka@tlen.pl>
2023-07-17 12:58:17 +0200

    feal(query): add new queries with `AVG()`

---
+++ queries/avg.sql
@@ -0,0 +1,48 @@
+-- AVG() - CustomersCustomers
+SELECT * FROM Customers;
+
+SELECT AVG(PostalCode) AS AverageOfPostalCode 
+FROM Customers;
+
+SELECT City, AVG(PostalCode)
+FROM Customers
+GROUP BY City;
+
+-- ----------------------------------
+-- AVG() - OrderItems
+SELECT * FROM OrderItems;
+
+SELECT Product, AVG(Price)
+FROM OrderItems;
+
+SELECT AVG(DISTINCT Price)
+FROM OrderItems;
+
+SELECT OrderID, AVG(Price)
+FROM OrderItems
+GROUP BY OrderID;
+
+-- ----------------------------------
+-- AVG() - Orders
+SELECT * FROM Orders;
+
+SELECT AVG(TotalAmount)
+FROM Orders;
+
+SELECT CustomerID, AVG(DISTINCT TotalAmount)
+FROM Orders
+GROUP BY CustomerID;
+
+-- ----------------------------------
+-- AVG() - Products
+SELECT * FROM Products;
+
+SELECT AVG(UnitPrice) AS AverageOfUnitPrice
+FROM Products;
+
+SELECT AVG(DISTINCT UnitPrice)
+FROM Products;
+
+SELECT AVG(DISTINCT UnitPrice), QuantityInStock
+FROM Products
+GROUP BY QuantityInStock;
